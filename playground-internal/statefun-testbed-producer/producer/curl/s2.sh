73aadd8e488e9f02b5fd9d287f5675b0158968e8
Jiaxiang Yu <yujiaxiang2013@gmail.com>
2022-11-16 16:11:07 +0800

    changes for demo v0

--- playground-internal/statefun-testbed-producer/producer/curl/s2.sh
+++ playground-internal/statefun-testbed-producer/producer/curl/s2.sh
@@ -1,8 +1,8 @@
 python3 producer.py --restock --item_id socks --quantity 50 --stream_id s2
-sleep 1
+sleep 2
 
 python3 producer.py --add_to_cart --user_id u1 --item_id socks --quantity 50 --stream_id s2
-sleep 1
+sleep 2
 
 python3 producer.py --checkout --user_id u1 --stream_id s2
 sleep 1
--- playground-internal/statefun-testbed-producer/producer/curl/trigger.sh
+++ playground-internal/statefun-testbed-producer/producer/curl/trigger.sh
@@ -1,3 +1,7 @@
+# clear the previous outputs
+rm -f *.err *.out
+sleep 1
+
 ./s1.sh > s1.out 2> s1.err &
 echo "stream 1 started"
 ./s2.sh > s2.out 2> s2.err &
